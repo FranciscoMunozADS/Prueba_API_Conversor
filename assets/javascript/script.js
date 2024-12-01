8fd10158875f1d38f8d188bf23ad8e693f1c7a5b
Francisco Muñoz <munozjaraf@gmail.com>
2024-12-01 20:35:42 -0300

    canas verdes con el gráfico

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -33,7 +33,8 @@ document.getElementById("btnConvert").addEventListener('click', async () => {
     const amountInput = document.getElementById('amountInput');
     const currencySelect = document.getElementById('currencySelect');
     const amount = parseFloat(amountInput.value); // Convierte cualquier 'string' dentro del value a número
-    const selectedCurrency = currencySelect.value;
+    const selectedCurrency = currencySelect.value; 
+    const currencyName = currencySelect.options[currencySelect.selectedIndex].text;
 
     /* validar */
 
@@ -50,7 +51,7 @@ document.getElementById("btnConvert").addEventListener('click', async () => {
         return;
        }
 
-
+       
     /* Conversión según datos */
 
     const conversionRate = rates[selectedCurrency].valor; // Selecciona el valor de rates dentro de cada divisa seleccionada
@@ -59,12 +60,76 @@ document.getElementById("btnConvert").addEventListener('click', async () => {
     /* Mostrar resultado en DOM */
     const resultText = document.getElementById("resultValue");
     resultText.textContent = `${convertedAmount.toFixed(2)} ${selectedCurrency}` //toFixed asegura solo (dos) decimales
+
+    
+    renderChart(selectedCurrency, rates, currencyName);
     } else {
+        const resultText = document.getElementById("resultValue");
         resultText.textContent = `Ha ocurrido un error.`;
         resultText.style.color = "red";
         resultText.style.fontWeight = "bold"
     }
+
+    
 });
 
 
+/* Gráfico */
+
+const chartDOM = document.getElementById("currencyChart");
+
+let currencyChart = ""; // variable global
+const currencyName = currencySelect.options[currencySelect.selectedIndex].text;
+const getHistoricalData = async (selectedCurrency) => {
+    const respuesta = await fetch(`https://mindicador.cl/api/${selectedCurrency}`);
+    const data = await respuesta.json();
+
+    console.log('Datos históricos recibidos:', data);//para verificar
+
+    /* se extraen las últimas 10 fechas y valores (splice) [Datos Históricos] */
+
+    const dates = data.serie.slice(0, 10).map((entry) => entry.fecha.split("T")[0]); // 'T' representa el formato de la fecha 
+
+    const values = data.serie.slice(0, 10).map((entry) => entry.valor);
+
+    return {dates, values};
+
+}
+
+/* configuración gráfico */
+
+const chartConfigPrep = (dates, values, currencyName) => {
+
+    const config = {
+        type: "line",
+        data: {
+            labels: dates,
+            datasets:[{
+                label: `Últimos 10 dias de ${currencyName}`,
+                data: values,
+                borderColor: "blue",
+            }]
+        }
+    }
+
+    return config;
+    
+}
+
+/* renderizar gráfico */
+
+const renderChart = async (selectedCurrency, rates, currencyName) => {
+    const historicalData = await getHistoricalData(selectedCurrency);
+    
+    if (historicalData) {
+        const {dates, values} = historicalData;
+
+        if (currencyChart) {
+            currencyChart.destroy(); // Si currencyChart = true, se elimina para dar paso al siguiente
+        }
+
+        const config = chartConfigPrep(dates, values, currencyName);
+        currencyChart = new Chart(chartDOM, config);
+    }
+}
 
